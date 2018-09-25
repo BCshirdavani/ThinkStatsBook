fb42e7268f661feec9b04415f4c64633a5f4e345
shiMac <beau.shirdavani@bellevuecollege.edu>
2018-09-24 22:57:13 -0700

    started chapter 12
    pp.147

---
+++ ch12_timeSeries.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 24 21:58:16 2018
+
+@author: shimac
+"""
+import pandas
+transaction = pandas.read_csv('/Users/shimac/Documents/PythonBooks/ThinikStats/ThinkStatsBook/data/mj-clean.csv', parse_dates = [5])
+
